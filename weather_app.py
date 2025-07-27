e4887b34419f746a7ab9e8465185ac92a2a7b2a2
Emma Spear <emmspear@gmail.com>
2025-07-27 17:40:02 +0800

    removed emoji

--- weather_app.py
+++ weather_app.py
@@ -10,7 +10,7 @@ st.set_page_config(
 )
 
 #Page title and description
-st.title("Weather Data Analyzer 🌡️")
+st.title("Weather Data Analyzer")
 st.write("This app analyzes weather data from CSV files")
 
 #Selection header
